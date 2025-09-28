4508af9080587703e54fb6eeafa0c05d592a9be4
Eracles <camposzavalaeracles@gmail.com>
2025-09-27 19:57:12 -0600

    Añadido estilos a la pagina de detalles de peliculas

--- app/frontend/src/pages/MovieDetailPage/MovieDetailPage.jsx
+++ app/frontend/src/pages/MovieDetailPage/MovieDetailPage.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import Loader from "../../components/loader/loader"
 import { getMovie } from "../../Utils/Movies/Movies"
+import "./MovieDetailPage.css"
 const MovieDetailPage = ()=>{
     const {slug, id} = useParams()
     const {movie, error, loading} = getMovie({slug, id})
@@ -14,17 +15,30 @@ const MovieDetailPage = ()=>{
         )
     }
     return(
-        <div className="movie-detail-container">
-            <h1>Movie Detail</h1>
+        <div className="movie-detail-container" style={{backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0) 70%, rgba(0,0,0,1) 100%), url('http://localhost:3000/${movie ? movie.poster : ''}')`}}>
             {!error && movie && (
-                <div>
-                    <img src={`http://localhost:3000/${movie.poster}`} alt={movie.name} />
-                    <h2>{movie.name}</h2>
-                    <h3>Descripcion: </h3><p>{movie.description}</p>
-                    <p>Clasificacion: <strong>{movie.clasification}</strong></p>
-                </div>
+                <>
+                    <div className="movie-detail-card">
+                        <div className="movie-detail-poster">
+                            <img src={`http://localhost:3000/${movie.poster}`} alt={movie.name} />
+                        </div>
+                        <div className="movie-detail-info">
+                            <div>
+                                <h2>{movie.name}</h2>
+                                <span>{movie.duration} min</span>
+                            </div>
+                            <div className="movie-detail-actions">
+                                <Link to={`/booking/${movie.slug}/${movie.id}`} className="button-detail-booking">Reservar</Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="movie-detail-sinopsis">
+                        <h3>Sinopsis</h3>
+                        <p>{movie.description}</p>
+                    </div>
+                </>
             )}
-            {error && <p>{error}</p>}
+            {error && <p className="error-message">{error}</p>}
         </div>
     )
 }
